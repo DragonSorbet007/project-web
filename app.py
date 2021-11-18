8390fd9b4d9fc02c768c2f5bc9c5da6622d11eaa
DragonSorbet007 <karri.lehtovuori@gmail.com>
2021-11-18 13:56:57 +0000

    Ratings engine working

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def movie():
     with sqlite3.connect("db/movie.db") as c:
-        return render_template('index.html', suggests=c.execute("""
+        return render_template('index.html', api_url="http://127.0.0.1:8000", suggests=c.execute("""
             SELECT ItemID, MovieTitle, ROUND(Rating, 1) FROM movies 
             ORDER BY Rating DESC
             LIMIT 50 
@@ -17,4 +17,4 @@ def movie():
 
 @app.route("/sample")
 def movie_sample():
-    return render_template('index.html', suggests=[(1, "Transformers", 3.5), (2, "Annie Hall", 4), (3, "Toy Story", 4.5)])
+    return render_template('index.html', api_url="http://127.0.0.1:8000", suggests=[(1, "Transformers", 3.5), (2, "Annie Hall", 4), (3, "Toy Story", 4.5)])
